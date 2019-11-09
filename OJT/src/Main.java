e4b99209d1a18634c2d766a3250ab810a51b2577
Esteban21 <isomorphism1126@gmail.com>
2019-11-09 15:48:43 +0900

    test

--- OJT/src/Main.java
+++ OJT/src/Main.java
@@ -1,6 +1,26 @@
+class Calculator{
+    int left, right;
+
+    public void setOperands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+
+}
+
+class SubstractionableCalculator extends Calculator {
+    public void substract() {
+        System.out.println(this.left - this.right);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("End");
     }
 }
